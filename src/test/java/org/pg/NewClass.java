93119bc4b3a876ca4c50aa63145726e0613fea36
Saranya <saranya3606@gmail.com>
2022-11-11 16:04:02 +0530

    First Code

--- src/test/java/org/pg/NewClass.java
+++ src/test/java/org/pg/NewClass.java
@@ -1,5 +1,40 @@
 package org.pg;
 
 public class NewClass {
+   private void sec1() {
+	   System.out.println("abc");
+}
+	
+   private void sec2() {
+	   System.out.println("abcd");	
 
+	}
+	
+   private void sec3() {
+	   System.out.println("amma");	
+
+	}
+	
+	private void secA() {
+		System.out.println("appa");
+
+	}
+	
+	private void secB() {
+		System.out.println("akka");
+
+	}
+	
+	private void secC() {
+		
+		System.out.println("mama");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
